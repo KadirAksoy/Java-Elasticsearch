6f560a945b1b00ffcbcfcd1a463db54e33e2804f
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-10-03 16:01:29 +0300

    config  added

---
+++ src/main/java/com/kadiraksoy/JavaElasticsearch/config/ESConfig.java
@@ -0,0 +1,25 @@
+package com.kadiraksoy.JavaElasticsearch.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackageClasses = "com.kadiraksoy.JavaElasticsearch.repository")
+@ComponentScan(basePackageClasses = "com.kadiraksoy.JavaElasticsearch")
+public class ESConfig extends ElasticsearchConfiguration {
+
+    @Value("${elasticsearch.url}")
+    private String url;
+
+    @Override
+    public ClientConfiguration clientConfiguration(){
+        return ClientConfiguration.builder()
+                .connectedTo(url)
+                .build();
+    }
+}
