b11652e3f453e16a970d2be3ab6e3cfc9ba9c3ab
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-10-03 15:52:47 +0300

    Initial commit

---
+++ src/main/java/com/kadiraksoy/JavaElasticsearch/JavaElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.JavaElasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaElasticsearchApplication.class, args);
+	}
+
+}
